ea08daab0b275e11c5fcf512520c172a0de761a3
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2026-03-01 00:35:22 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import express from 'express';
 import { initDB } from './config/db.js';
 import rateLimiterMiddleware from './middleware/rateLimiter.js';
+import transactionsRoute from './routes/transactionsRoutes.js';
 
 const app = express();
 
@@ -10,11 +12,11 @@ app.use(rateLimiterMiddleware)
 const PORT = process.env.PORT || 5009;
 
 
+app.use("/api/transactions", transactionsRoute);
 
-
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the server!' });
-});
+// app.get('/api/data', (req, res) => {
+//   res.json({ message: 'Hello from the server!' });
+// });
 
 initDB().then(() => {
 app.listen(PORT, () => {
